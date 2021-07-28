e47e46597b5f2d882bef7a6cd195d6405522450c
solitary-flower <lpg544@gmail.com>
2021-07-28 11:00:13 -0400

    Added base type to users, created blog post POST route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/user');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
-// @route  POST api/posts
+// @route  Profile api/posts
 // @desc   Create a post
 // @access Private (Admin only)
 router.post(
@@ -13,8 +16,49 @@ router.post(
         [
             check('text', 'Text is required').not().isEmpty(),
             check('title', 'Title is required').not().isEmpty(),
-            check('type', 'Must be administrator').isIn('administrator'),
         ],
     ],
-    async (req, res) => {}
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const user = await User.findById(req.user.id).select('-password');
+        if (user.type != 'administrator') {
+            return res
+                .status(404)
+                .send('Must be administrator to make blog post.');
+        }
+        try {
+            const newPost = new Post({
+                text: req.body.text,
+                title: req.body.title,
+                tags: req.body.tags,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id,
+            });
+
+            const post = await newPost.save();
+            res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
 );
+
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Public
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
