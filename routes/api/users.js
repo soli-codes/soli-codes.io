16a7a93cdab1aac5408117fc0806ff17ff189188
solitary-flower <lpg544@gmail.com>
2021-08-30 15:51:16 -0400

    trying again :)))

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const User = require('../../models/User');
+const User = require('../../models/user');
 
 // @route  POST api/users
 // @desc   Register user
